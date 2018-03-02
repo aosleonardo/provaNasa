a6da1cf6bcbbe65f3059ed362befbd31eaca2d39
leo <leo@leo-debian>
2018-03-01 23:06:51 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/teste/contaazul/contaazul/ContaazulApplication.java
@@ -0,0 +1,12 @@
+package br.com.teste.contaazul.contaazul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaazulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaazulApplication.class, args);
+	}
+}
